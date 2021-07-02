44dcc07af1ea4ce99629044bc4d55b49a0155373
Xudoyberdi123 <xudikk.1@gmail.com>
2021-07-02 16:46:54 +0500

    recipe obrazez

---
+++ src/dashboard/receipe/views.py
@@ -0,0 +1,16 @@
+from front.models import Ingredient
+
+
+class RecipeView:
+    def list(self):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def detail(self):
+        pass
+
